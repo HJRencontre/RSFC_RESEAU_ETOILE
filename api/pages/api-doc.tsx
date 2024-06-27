51a51994720aecb3830e15f07fc8807eebf6ae03
Hermann-Jude Rencontre <h.jrencontre@outlook.com>
2024-06-27 11:06:24 +0200

    hugo enfoiré

---
+++ api/pages/api-doc.tsx
@@ -0,0 +1,64 @@
+import {InferGetStaticPropsType} from 'next';
+import {createSwaggerSpec} from 'next-swagger-doc';
+import dynamic from 'next/dynamic';
+import 'swagger-ui-react/swagger-ui.css';
+
+const SwaggerUI = dynamic<{
+  spec: any;
+}>(import('swagger-ui-react'));
+
+export default function ApiDoc({spec}: InferGetStaticPropsType<typeof getStaticProps>) {
+  return <SwaggerUI spec={spec}/>;
+}
+
+export const getStaticProps: () => Promise<{ props: { spec: Record<string, any> } }> = async () => {
+  const spec: Record<string, any> = createSwaggerSpec({
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Next Swagger API Documentation',
+        version: '1.0',
+      },
+      components: {
+        securitySchemes: {
+          bearerAuth: {
+            type: 'http',
+            scheme: 'bearer',
+            bearerFormat: 'JWT',
+          },
+        },
+      },
+      security: [
+        {
+          bearerAuth: [],
+        },
+      ],
+      tags: [
+        {
+          name: 'users',
+        },
+        {
+          name: 'partners',
+        },
+        {
+          name: 'partners_contacts',
+        },
+        {
+          name: 'offers',
+        },
+        {
+          name: 'events',
+        },
+        {
+          name: 'business_segments',
+        }
+      ],
+    },
+  });
+
+  return {
+    props: {
+      spec,
+    },
+  };
+};
